0dee74033dc115e537657b7c92e77ed932b10afb
Alec Gordon <insanealec@gmail.com>
2023-12-06 16:52:14 -0500

    Convert display lists to indexed collections
    - Solves issue of dupicates causing each other to be checked when switching items betwen lists
    - Now ready stays unchecked and complete stays checked
    - Signify the differences between 'database' types and the display list type
    - Simplifies the list management a bit since we can just swap stuff easy now
    - Make styling slightly nicer

--- src/components/betterlist/BetterList.vue
+++ src/components/betterlist/BetterList.vue
@@ -7,11 +7,17 @@ interface Item {
   categoryID: string;
   isAssumedCategory: boolean;
 }
+// "DB" for all items
 interface ItemList {
   [id: string]: Item;
 }
+// "DB" for terms
 interface TermList {
   [name: string]: string;
+}
+// Type for active list of items
+interface ListGroup {
+  [id: string]: string;
 }
 
 interface Category {
@@ -30,10 +36,11 @@ const DEFAULT_CATEGORY: Category = {
 let term = ref('');
 const termList = ref({} as TermList);
 const itemList = ref({} as ItemList);
-const readyList = ref([] as Item[]);
-const completeList = ref([] as Item[]);
+const readyList = ref({} as ListGroup);
+const completeList = ref({} as ListGroup);
 
 const addTerm = () => {
+  // No empty list terms
   if (!term.value) return;
   // Check if Item term already exists
   let item: Item;
@@ -50,54 +57,69 @@ const addTerm = () => {
     itemList.value[item.id] = item;
     termList.value[term.value] = item.id;
   }
-  readyList.value.push(item);
+  // Lists can have multiples of the item
+  readyList.value[crypto.randomUUID()] = item.id;
   term.value = '';
   console.log(readyList.value, termList.value, itemList.value)
 }
 
-const addReady = (item: Item, index: number) => {
-  readyList.value.push(item);
-  completeList.value.splice(index, 1);
+const addReady = (key: string) => {
+  readyList.value[key] = completeList.value[key];
+  delete completeList.value[key];
 }
 
-const addComplete = (item: Item, index: number) => {
-  completeList.value.push(item);
-  readyList.value.splice(index, 1);
+const addComplete = (key: string) => {
+  completeList.value[key] = readyList.value[key];
+  delete readyList.value[key];
 }
 
 </script>
 
 <template>
-<div className="flex min-h-screen flex-col">
-  <div className="flex flex-col w-full">
-    <h2 className="w-full">Betterlist</h2>
-    <form className="flex justify-between" @submit.prevent="addTerm">
-      <input type="text" list="terms" name="term" className="text-black" v-model.trim="term" />
+<div class="flex min-h-screen flex-col">
+  <div class="flex flex-col w-full">
+    <h2 class="w-full">Betterlist</h2>
+    <form class="flex" @submit.prevent="addTerm">
+      <input type="text" list="terms" name="term" class="text-black p-2" v-model.trim="term" />
       <datalist id="terms">
         <option v-for="(id, term) in termList" :key="id" :value="term">{{ term }}</option>
       </datalist>
-      <input type="submit" value="Submit" />
+      <button type="submit" class="py-2 px-3 bg-cyan-500 text-white text-xt-sm font-semibold">Add</button>
     </form>
   </div>
-  <div className="flex w-full h-auto">
-    <div className="flex flex-col w-1/2 border-r pr-1">
-      <h3 className="w-full">List</h3>
-      <div className="w-full flex flex-col">
-        <label v-for="(item, index) in readyList" :key="index">
-          <input type="checkbox" :checked="false" :name="item.name" :id="item.id" @change.prevent="addComplete(item, index)" />
-          {{ item.name }}
+  <div class="flex w-full h-auto">
+    <div class="flex flex-col w-1/2 border-r pr-1">
+      <h3 class="w-full">List</h3>
+      <div class="w-full flex flex-col">
+        <label class="flex flex-row items-center" v-for="(itemID, key) in readyList" :key="key">
+          <input class="item" type="checkbox" :checked="false" :name="itemList[itemID].name" :id="itemID" @change.prevent="addComplete(key.toString())" />
+          {{ itemList[itemID].name }}
         </label>
       </div>
     </div>
-    <div className="flex flex-col w-1/2 border-l pl-1">
-      <h3 className="w-full">Complete</h3>
-      <div className="w-full flex flex-col">
-        <label v-for="(item, index) in completeList" :key="index">
-          <input type="checkbox" :checked="false" :name="item.name" :id="item.id" @change.prevent="addReady(item, index)" />
-          {{ item.name }}
+    <div class="flex flex-col w-1/2 border-l pl-1">
+      <h3 class="w-full">Complete</h3>
+      <div class="w-full flex flex-col">
+        <label class="flex flex-row items-center" v-for="(itemID, key) in completeList" :key="key">
+          <input class="item" type="checkbox" :checked="true" :name="itemList[itemID].name" :id="itemID" @change.prevent="addReady(key.toString())" />
+          {{ itemList[itemID].name }}
         </label>
       </div>
     </div>
   </div>
 </div>
 </template>
+
+<style lang="scss" scoped>
+.item {
+  width: 1rem;
+  height: 1rem;
+  margin: 0.25rem;
+  border-radius: 0.25rem;
+
+  &:checked {
+    @apply shadow-cyan-500;
+    box-shadow: inset 0 0 0 1rem var(--tw-shadow-color);
+  }
+}
+</style>
