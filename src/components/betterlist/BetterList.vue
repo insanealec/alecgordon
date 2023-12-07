ad092ed730b516013ad600b0741521acbbd9a5a4
Alec Gordon <insanealec@gmail.com>
2023-12-07 16:01:31 -0500

    Option to select category
    - No way to add categories yet so it's functionally the same as when it just set them all to the default category
    - Category can be selected on adding or when editing Items can happen

--- src/components/betterlist/BetterList.vue
+++ src/components/betterlist/BetterList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 interface Item {
   id: string;
@@ -30,14 +30,25 @@ interface CategoryList {
 }
 const DEFAULT_CATEGORY: Category = {
   id: '0000-0000-0000-0000-0000',
-  name: 'Unknown',
+  name: 'No Category',
 }
 
 let term = ref('');
+let selectedCategory = ref(DEFAULT_CATEGORY.id);
 const termList = ref({} as TermList);
 const itemList = ref({} as ItemList);
 const readyList = ref({} as ListGroup);
 const completeList = ref({} as ListGroup);
+const categoryList = ref({} as CategoryList);
+
+onMounted(() => {
+  // TODO: load categories from storage
+  if (localStorage.getItem('categories')) {
+    //
+  } else {
+    categoryList.value[DEFAULT_CATEGORY.id] = DEFAULT_CATEGORY;
+  }
+});
 
 const addTerm = () => {
   // No empty list terms
@@ -50,8 +61,7 @@ const addTerm = () => {
     item = {
       id: crypto.randomUUID(),
       name: term.value,
-      // TODO: analyze term and try to match to category
-      categoryID: DEFAULT_CATEGORY.id,
+      categoryID: selectedCategory.value,
       isAssumedCategory: true,
     };
     itemList.value[item.id] = item;
@@ -84,6 +94,9 @@ const addComplete = (key: string) => {
       <datalist id="terms">
         <option v-for="(id, term) in termList" :key="id" :value="term">{{ term }}</option>
       </datalist>
+      <select v-model="selectedCategory" class="text-black p-2">
+        <option v-for="(category, id) in categoryList" :value="id" :key="id">{{ category.name }}</option>
+      </select>
       <button type="submit" class="py-2 px-3 bg-cyan-500 text-white text-xt-sm font-semibold">Add</button>
     </form>
   </div>
