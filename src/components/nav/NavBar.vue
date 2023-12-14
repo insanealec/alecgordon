30606e193a1f05230b47e017f18bb4ca0dd9d6a2
Alec Gordon <insanealec@gmail.com>
2023-12-14 12:09:32 -0500

    FIx up light/dark dropdown colors
    - Need to change link colors for light mode

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -9,7 +9,7 @@ import ThemeToggle from './ThemeToggle.vue';
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-30 p-2 shadow bg-indigo-200 dark:bg-slate-800 rounded-box w-52">
         <li><RouterLink :to="{ name: 'experiences' }">Experiences</RouterLink></li>
         <li>
           <summary>Side-projects</summary>
@@ -28,7 +28,7 @@ import ThemeToggle from './ThemeToggle.vue';
       <li>
         <details>
           <summary>Side-projects</summary>
-          <ul class="p-2 z-30">
+          <ul class="p-2 z-30 bg-indigo-200 dark:bg-slate-800">
             <li><RouterLink to="/betterlist">BetterList</RouterLink></li>
           </ul>
         </details>
