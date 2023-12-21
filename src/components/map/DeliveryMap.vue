ded3215eb3ed16bc66df00ae1ae3ecb5b2f6cfbc
Alec Gordon <insanealec@gmail.com>
2023-12-20 21:03:39 -0500

    Map changes style with darkmode
    - Will need to replace all elements on the map when the style changes
    - Add blur/un-interactibility to locked api input, for fun
    - Being able to useDark anywhere and watch it for changes is so freaking nice, no need for separate eventbus (wouldn't work in vue3 anyway)

--- src/components/map/DeliveryMap.vue
+++ src/components/map/DeliveryMap.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useDark, useToggle } from '@vueuse/core';
 import mapboxgl from 'mapbox-gl';
 
 const isLocked = ref(false);
 const toggleLock = useToggle(isLocked);
+const lockStyle = computed(() => isLocked.value ? 'pointer-events-none select-none blur-sm' : '');
+
+const isDark = useDark();
+const mapStyle = computed(() => `mapbox://styles/mapbox/navigation-${ isDark.value ? 'night' : 'day' }-v1`);
+// Change the map style when darkmode is toggled
+watch(isDark, () => {
+  if (map.value) {
+    // `diff: true` tries to only change the style and keep everything, but it prints warnings every time
+    map.value.setStyle(mapStyle.value, { diff: false });
+    // TODO: re-add markers/elements to map (https://docs.mapbox.com/mapbox-gl-js/example/style-switch/)
+  }
+});
 
 const accessToken = ref('');
 const map = ref();
@@ -25,8 +37,7 @@ const init = () => {
   mapboxgl.accessToken = accessToken.value;
   map.value = new mapboxgl.Map({
     container: mapBox.value,
-    // Is there a way to toggle only the style when darkmode changes without reinstantiating the map?
-    style: `mapbox://styles/mapbox/navigation-${ useDark().value ? 'night' : 'day' }-v1`,
+    style: mapStyle.value,
     // long,lat
     center: [-71.224518, 42.213995],
     zoom: 9,
@@ -38,7 +49,7 @@ const init = () => {
 <form class="flex flex-row mb-1" @submit.prevent="submit">
   <div class="flex flex-col w-full">
     <label for="token" class="text-sm font-medium text-gray-900 dark:text-white">MapBox API Token</label>
-    <input type="text" id="token" name="token" placeholder="Token" v-model.trim="accessToken" :disabled="isLocked" />
+    <input type="text" id="token" name="token" placeholder="Token" :class="lockStyle" v-model.trim="accessToken" :disabled="isLocked" />
   </div>
   <button type="submit" class="whitespace-nowrap py-2 px-3 bg-cyan-500 text-white font-semibold">{{ isLocked ? 'Clear' : 'Initialize' }} Map</button>
 </form>
