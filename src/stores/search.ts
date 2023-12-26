0dcc0ff091f0c95f220dc17b3afa5cc300082241
Alec Gordon <insanealec@gmail.com>
2023-12-26 17:55:56 -0500

    Doesn't suggest if no term
    - Move try/catch out of store
    - Add getter for seeing if suggestions is empty

--- src/stores/search.ts
+++ src/stores/search.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 import { type Coordinates, toCoords } from './map';
-import { filter } from 'lodash';
+import { filter, isEmpty } from 'lodash';
 
 interface Suggestion {
   id: string,
@@ -20,35 +20,33 @@ export const useSearchStore = defineStore('search', () => {
   const query = computed(() => encodeURIComponent(term.value));
   const tokenStr = computed(() => `session_token=${SESSION_TOKEN}&access_token=${accessToken.value}`);
   const suggestions = ref({} as SuggestionList);
+  const hasSuggestions = computed(() => !isEmpty(suggestions.value));
 
   const suggest = async (proximity?: Coordinates) => {
+    // If the search bar is clear, also clear suggestions
+    if (isEmpty(query.value)) {
+      suggestions.value = {} as SuggestionList;
+      return;
+    }
     let url = `${SEARCH_API}suggest?q=${query.value}&${tokenStr.value}`;
     if (proximity) {
       url = `${url}&proximity=${proximity.lng},${proximity.lat}`;
     }
-    try {
-      const request = await fetch(url);
-      // TODO: Place response.attribution somewhere for legal
-      const response = await request.json();
-      // Remove Brand type and anything without a full_address
-      const filteredSuggestions = filter(response.suggestions, (s) => s.feature_type !== 'brand' && s.full_address);
-      suggestions.value = filteredSuggestions.reduce((acc, s): SuggestionList => {
-        return { ...acc, [s.mapbox_id]: { id: s.mapbox_id, address: s.full_address, } };
-      }, {});
-    } catch (e) {
-      console.log('suggest', e)
-    }
+    const request = await fetch(url);
+    // TODO: Place response.attribution somewhere for legal
+    const response = await request.json();
+    // Remove Brand type and anything without a full_address
+    const filteredSuggestions = filter(response.suggestions, (s) => s.feature_type !== 'brand' && s.full_address);
+    suggestions.value = filteredSuggestions.reduce((acc, s): SuggestionList => {
+      return { ...acc, [s.mapbox_id]: { id: s.mapbox_id, address: s.full_address, } };
+    }, {});
   }
 
-  const retrieve = async (id: string): Promise<Coordinates|undefined> => {
+  const retrieve = async (id: string): Promise<Coordinates> => {
     const url = `${SEARCH_API}retrieve/${id}?${tokenStr.value}`;
-    try {
-      const request = await fetch(url);
-      const response = await request.json();
-      return toCoords(response.features[0].geometry.coordinates);
-    } catch (e) {
-      console.log('retrieve', e);
-    }
+    const request = await fetch(url);
+    const response = await request.json();
+    return toCoords(response.features[0].geometry.coordinates);
   }
 
   return {
@@ -57,6 +55,7 @@ export const useSearchStore = defineStore('search', () => {
     query,
     tokenStr,
     suggestions,
+    hasSuggestions,
     suggest,
     retrieve,
   }
