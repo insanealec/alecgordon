666a45824dc67bba453b7e0cad815c60dfc4fba6
Alec Gordon <insanealec@gmail.com>
2024-01-07 00:35:28 -0500

    Count al instances of cuisines to weight matches

--- src/stores/food.ts
+++ src/stores/food.ts
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia';
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 
 interface Person {
   name: string;
+  cuisines: string[];
 }
 
 export type {
@@ -22,6 +23,7 @@ const CUISINES = [
 
 const INITIAL_PERSON: Person = {
   name: 'Person 1',
+  cuisines: [],
 }
 const MAX_PEOPLE = 10;
 
@@ -37,6 +39,7 @@ export const useFoodStore = defineStore('food', () => {
     if (len < MAX_PEOPLE) {
       const person: Person = {
         name: `Person ${len+1}`,
+        cuisines: [],
       }
       people.value.push(person);
       return len;
@@ -44,9 +47,23 @@ export const useFoodStore = defineStore('food', () => {
     return len - 1;
   }
 
+  const matches = () => {
+    // Mark each instance of a chosen cuisine
+    const instances: { [name: string]: number } = {};
+    for (const person of people.value) {
+      for (const cuisine of person.cuisines) {
+        if (!instances[cuisine]) instances[cuisine] = 0;
+        instances[cuisine]++;
+      }
+    }
+    console.log(instances)
+    return instances;
+  }
+
   return {
     people,
     addPerson,
+    matches,
   };
 });
 
