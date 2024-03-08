89434af401fe95d9a59fa162108a7b16e76afb9d
Alec Gordon <insanealec@gmail.com>
2024-03-08 14:37:28 -0500

    I didn't commit this last time, so I'm doing that now while I prep for challenges

---
+++ src/components/food/CuisineMap.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const mapBox = ref();
+
+</script>
+
+<template>
+<div>
+  <div ref="mapBox" class="map-box flex flex-col min-h-screen max-w-screen"></div>
+</div>
+</template>
+
+<style lang="scss">
+.mapboxgl-canvas {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
